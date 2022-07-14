17c352a30fa27f7b79c660dedab6cccb8e211357
gaston lista <gastonmalet@hotmail.com>
2022-07-13 21:48:27 -0300

    Se agrego fetchen Contacto

---
+++ js/correo.js
@@ -0,0 +1,11 @@
+document.getElementById('alert').onclick = function(){
+Swal.fire({
+    title: 'Gracias por Contactarnos!',
+    text: 'te escribiremos pronto :)',
+    icon: 'success',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'correo',
+  })
+
+}
